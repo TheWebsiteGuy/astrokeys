fc446bd8fdd352c8ef63054a7b0bdbdc3ee7021a
Patrick (The Website Guy) <patrick@thewebsiteguy.uk>
2024-07-10 13:05:30 +0100

    sitename in settings added to navbar

--- src/cms/singletons/settings.ts
+++ src/cms/singletons/settings.ts
@@ -2,6 +2,7 @@ import { fields, singleton } from '@keystatic/core';
 
 export const settings = singleton({
     label:  'Settings',
+    path: "src/content/pages/settings",
     schema:{
         site: fields.object({
            siteName: fields.text({ label: 'Site Name' })
