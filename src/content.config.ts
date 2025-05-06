f22c05e245d9461a8467463c560742540a5274d6
Patrick (The Website Guy) <patrick@thewebsiteguy.uk>
2025-05-06 15:01:05 +0100

    Remove SEO collection and update SEO settings in JSON configuration

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content'
 import { glob, file } from 'astro/loaders'
 
 const posts = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.{md,mdx,mdoc,yaml}', base: './src/content/posts' }),
+  loader: glob({ pattern: '**/[^_]*.(md|mdx|mdoc|yaml|json)', base: './src/content/posts' }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
@@ -13,7 +13,7 @@ const posts = defineCollection({
 })
 
 const pages = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.{md,mdx,mdoc,yaml,json}', base: './src/content/pages' }),
+  loader: glob({ pattern: '**/[^_]*.(md|mdx|mdoc|yaml|json)', base: './src/content/pages' }),
   schema: ({ image }) =>
     z.object({
       hero: z.object({
@@ -27,15 +27,28 @@ const pages = defineCollection({
             label: z.string(),
             link: z.string(),
             icon: z.string().optional(),
-            style: z.enum(['primary', 'link', 'secondary', 'danger', 'success', 'warning', 'info', 'outline', 'neutral', 'ghost', '']).optional(),
-            size: z.enum(['small', 'large', 'medium', 'tiny', 'wide', 'default', '' ]).optional(),
+            style: z
+              .enum([
+                'primary',
+                'link',
+                'secondary',
+                'danger',
+                'success',
+                'warning',
+                'info',
+                'outline',
+                'neutral',
+                'ghost',
+                '',
+              ])
+              .optional(),
+            size: z.enum(['small', 'large', 'medium', 'tiny', 'wide', 'default', '']).optional(),
           }),
         ),
       }),
       posts: z.object({
         heading: z.string(),
         subheading: z.string(),
-
       }),
     }),
 })
@@ -47,24 +60,6 @@ const branding = defineCollection({
   }),
 })
 
-const seo = defineCollection({
-  loader: file('./src/settings/seo.json'),
-  schema: ({ image }) => z.object({
-    title: z.string().optional(),
-    description: z.string().optional(),
-    keywords: z.string().optional(),
-    canonical: z.string().optional(),
-    opengraph: z.object({
-      Title: z.string().optional(),
-      Description: z.string().optional(),
-      Image: image().optional(),
-    }),
-    twitter: z.object({
-      Title: z.string().optional(),
-      Description: z.string().optional(),
-      Image: image().optional(),
-    }),
-  }), 
-})
 
-export const collections = { posts, branding, pages, seo }
+
+export const collections = { posts, branding, pages }
